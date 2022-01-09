3b2516f29a0c3059bd4e49fd392ad5063093ec96
gogopavl <pgogousis@gmail.com>
2022-01-09 00:05:55 +0000

    explain why the need for an alternative password reader

--- src/main/java/com/pvlrs/kesar/utility/StandardInputPasswordReader.java
+++ src/main/java/com/pvlrs/kesar/utility/StandardInputPasswordReader.java
@@ -4,10 +4,22 @@ import com.pvlrs.kesar.condition.StandardInputPasswordReaderCondition;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Component;
 
+import java.io.Console;
 import java.util.Scanner;
 
 import static com.pvlrs.kesar.constant.UserMessages.PASSWORD_PROMPT;
 
+/**
+ * <p> Ideally, the password should be read via {@link Console#readPassword(java.lang.String,
+ * java.lang.Object...)} as done in the {@link ConsolePasswordReader} implementation, but
+ * if there isn't any {@link Console} object associated with the current JVM, this won't
+ * work as {@link System#console()} will return {@code null} (for instance, when running
+ * through an IDE). </p>
+ *
+ * <p> This implementation is offered as an alternative to {@link ConsolePasswordReader} so
+ * that we are able to still read a password from the standard input. The main drawback with
+ * this method is that the password is visible in the console.</p>
+ */
 @Component
 @Conditional(StandardInputPasswordReaderCondition.class)
 public class StandardInputPasswordReader implements PasswordReader {
